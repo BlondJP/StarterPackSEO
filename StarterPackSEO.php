baaecaa029c2eaffa1ac3183e039ae3dc1cc3ef1
JP BLOND <jpb.blond@gmail.com>
2016-03-09 20:52:24 +0100

    avancement

--- StarterPackSEO.php
+++ StarterPackSEO.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 #############################################################################
@@ -7,29 +7,51 @@
 
 class StarterPackSEO
 {
+	private $keyword;
 
+	# DOM ELEMENTS
+	private $title;
 	private $h1;
 	private $h2s;
 	private $h3s;
 
 	public function __construct($keyword)
 	{
+		$this->keyword = $keyword;
 
+		$this->title = 'TITLE par default';
+		$this->h1 = 'H1 par default';
+		$this->h2s = [];
+		$this->h3s = [];
 	}
+
+	#################################################
+	####### METHODES POUR REMPLIR LE DOM ############
+	#################################################
 
 	public function setH1($h1)
 	{
-
+		$this->h1 = $h1;
 	}
 
-	public function setH2($h2Index, $h2)
+	public function setH2s($h2Key, $h2)
 	{
-
+		$this->h2s[$h2Key] = $h2;
 	}
+
+	public function setH3s($h3Key, $h3)
+	{
+		$this->h3s[$h3Key] = $h3;
+	}
+
+
+	#################################################
+	####### METHODES POUR AUDITER LE DOM ############
+	#################################################
 
 	public function getDom()
 	{
-
+		var_dump($this);
 	}
 
 	public function auditSEO()
